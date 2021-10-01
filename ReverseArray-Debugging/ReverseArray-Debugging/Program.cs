7c872dbd41b40b1e7f0c641dcd6f0eaf44b390ea
BlagoDamqnov <chessplayer89@abv.bg>
2021-10-01 15:32:34 +0300

    SortArrays

---
+++ ReverseArray-Debugging/ReverseArray-Debugging/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace ReverseArray_Debugging
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter length of array:");
+           int count = int.Parse(Console.ReadLine());
+
+            int []arr = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            Console.Write("The sorted array is: ");
+            foreach (var item in arr)
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
